eaf3a94d9382e41948dbd267f434e755e2cf31ed
Brandon <brandon@house.me>
2019-10-30 09:18:03 +0200

    Added initial commit

---
+++ LinqAwaiter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    public static partial class Enumerable
+    {
+        public static async Task<TSource[]> AwaiterAsync<TSource>(this IEnumerable<Task<TSource>> source)
+        {
+            //if (source == null)
+            //{
+            //    ThrowHelper.ThrowArgumentNullException(ExceptionArgument.source);
+            //}
+            var result = await Task.WhenAll(source);
+          
+            return result;
+        }
+    }
+}
